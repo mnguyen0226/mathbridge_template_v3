91f9818f781173393a231e5cccce3cae53d8287d
mnguyen0226 <mnguyen0226@vt.edu>
2023-05-21 19:51:34 -0400

    add main template

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Mathbridge Official 
 Data visualization webapp for Virginia Tech CS department machine learning course.
+
+## TODO
+- Add Logo
+- Add About + Author
+- Add hover tab instead of click tab
+- Instead of making the horizontal bar, what about we make a verticle bar?
--- app.py
+++ app.py
@@ -1,13 +1,59 @@
-from maindash import my_app
+# dash imports
+import dash
 from dash import html
+from dash import Input
+from dash import Output
+from dash import dcc
 
-# dash constructor
+# other imports
+import numpy as np
+import plotly.io as pio
+import plotly.express as px
+import pandas as pd
+import math
+import json
+
+# file imports
+from maindash import my_app
+from src.helper_functions.helpers import read_file_as_str
+
+#######################################
+# Initial Settings
+#######################################
+pio.templates
 server = my_app.server
+external_stylesheets = [
+    "https://codepen.io/chriddyp/pen/bWLwgP.css",
+    "assets/custom.css",
+]
 
-# dash layout
-my_app.layout = html.Div([
-    html.H3("Hello")
-])
+
+#######################################
+# Dash Layout
+#######################################
+my_app.layout = html.Div(
+    [
+        html.H2("Mathbridge Course"),
+        dcc.Tabs(
+            id="viz_tabs",
+            children=[
+                dcc.Tab(label="Random Expressions", value="rand_exp"),
+                dcc.Tab(label="Neural Networks", value="nn"),
+                dcc.Tab(label="Distributions", value="dist"),
+                dcc.Tab(label="Functions Plotted", value="functions"),
+                dcc.Tab(label="Eigen Vector & Eigen Values", value="eigen"),
+            ],
+            value="main_tab",
+        ),
+        html.Div(id="layout"),
+    ],
+    className="outer-div",
+)  # NOTE: className is for styling
+
+#######################################
+# Dash Callbacks
+#######################################
+
 
 if __name__ == "__main__":
-    my_app.run_server(debug=True,host='0.0.0.0',port=80)
+    my_app.run_server(debug=True, host="0.0.0.0", port=80)
