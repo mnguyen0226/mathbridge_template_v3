0e2659237d7c5ede165a3899b63dbb5f98802868
mnguyen0226 <mnguyen0226@vt.edu>
2023-05-25 17:01:50 -0400

    refactor tab 3

--- components/tab_3/func.py
+++ components/tab_3/func.py
@@ -1,18 +1,26 @@
+# dash imports
 from dash import dcc
 from dash import html
+from dash import Input
+from dash import Output
+
+# other imports
 import numpy as np
-from maindash import my_app
-from dash.dependencies import Input, Output
 import plotly.express as px
+
+# file imports
+from maindash import my_app
 from utils.ml import coor_gen
+from utils.others.file_operations import read_file_as_str
+from utils.stats.non_linear import square
+from utils.stats.non_linear import cube
+from utils.stats.non_linear import poly
+from utils.stats.non_linear import log
 
 
-def readFileasStr(filename):
-    with open(filename, "r") as file:
-        data = file.read()
-    return data
-
-
+#######################################
+# Dash Layout
+#######################################
 def func_layout():
     tab4Layout = html.Div(
         [
@@ -30,6 +38,25 @@ def func_layout():
     return tab4Layout
 
 
+#######################################
+# Dash Callbacks
+#######################################
+@my_app.callback(
+    Output(component_id="layout_tab4", component_property="children"),
+    Input(component_id="viz_funcs", component_property="value"),
+)
+def Tab4Render(ques):
+    if ques == "visualization":
+        return functionVizLayout()
+        # tab1_sel1_layout
+
+    if ques == "derivatives":
+        return functionDerivativeLayout()
+
+
+#######################################
+# Visualization Layout
+#######################################
 def functionVizLayout():
     tab4Layout = html.Div(
         [
@@ -47,10 +74,6 @@ def functionVizLayout():
                                 marks={data: str(data) for data in range(-20, 20, 2)},
                                 id="range_x",
                             ),
-                            # html.H4("Y-Range:"),
-                            # dcc.RangeSlider(min=-100, max=100, step=1, value=[-10, 10],
-                            #                 marks={data: str(data) for data in range(-10, 10, 2)},
-                            #                 id='range_y'),
                             html.H4("Select function"),
                             dcc.Dropdown(
                                 id="function-selection",
@@ -83,68 +106,9 @@ def functionVizLayout():
     return tab4Layout
 
 
-##############################3
-def functionDerivativeLayout():
-    tab5Layout = [
-        html.H4("Visualizing Derivatives"),
-        dcc.Dropdown(
-            options=[
-                {"label": "f(x)=x^2", "value": "square"},
-                {"label": "f(x)=x^3", "value": "cube"},
-                {"label": "f(x)=2x^2+4", "value": "poly"},
-                {"label": "f(x)=log(x)", "value": "log"},
-            ],
-            value="square",
-            id="fn-select-grad",
-        ),
-        html.Div(
-            [
-                html.Div(
-                    [html.Strong("Original Function"), dcc.Graph(id="functions-graph")],
-                    className="grouped-graphs",
-                ),
-                html.Div(
-                    [
-                        html.Strong("Function Derivative Graphed"),
-                        dcc.Graph(id="derivative-graph"),
-                    ],
-                    className="grouped-graphs",
-                ),
-            ],
-            className="row-pane",
-        ),
-        dcc.Markdown(
-            id="binom_block_md",
-            children=readFileasStr(
-                "./utils/markdown/tab_3/code_markdown_derivatives.md"
-            ),
-            className="code-markdown-view",
-            mathjax=True,
-        ),
-        html.Button(
-            "Download Code", id="btn-download-derivatives", className="btn-download"
-        ),
-        dcc.Download(id="download-derivatives"),
-    ]
-
-    return tab5Layout
-
-
-@my_app.callback(
-    Output(component_id="layout_tab4", component_property="children"),
-    Input(component_id="viz_funcs", component_property="value"),
-)
-def Tab4Render(ques):
-    if ques == "visualization":
-        return functionVizLayout()
-        # tab1_sel1_layout
-
-    if ques == "derivatives":
-        return functionDerivativeLayout()
-    return "The impaler!!!!"
-
-
-# Function Plotted Visualization Callback
+#######################################
+# Visualization Callbacks
+#######################################
 @my_app.callback(
     Output(component_id="function-plot", component_property="figure"),
     [Input("range_x", "value")],
@@ -181,23 +145,58 @@ def plotFunctions(range_x, function_selected):
     return fig
 
 
-# Function Plot Derivative Callbacks
-def square(x):
-    return x**2
+#######################################
+# Derivatives Layout
+#######################################
+def functionDerivativeLayout():
+    tab5Layout = [
+        html.H4("Visualizing Derivatives"),
+        dcc.Dropdown(
+            options=[
+                {"label": "f(x)=x^2", "value": "square"},
+                {"label": "f(x)=x^3", "value": "cube"},
+                {"label": "f(x)=2x^2+4", "value": "poly"},
+                {"label": "f(x)=log(x)", "value": "log"},
+            ],
+            value="square",
+            id="fn-select-grad",
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [html.Strong("Original Function"), dcc.Graph(id="functions-graph")],
+                    className="grouped-graphs",
+                ),
+                html.Div(
+                    [
+                        html.Strong("Function Derivative Graphed"),
+                        dcc.Graph(id="derivative-graph"),
+                    ],
+                    className="grouped-graphs",
+                ),
+            ],
+            className="row-pane",
+        ),
+        dcc.Markdown(
+            id="binom_block_md",
+            children=read_file_as_str(
+                "./utils/markdown/tab_3/code_markdown_derivatives.md"
+            ),
+            className="code-markdown-view",
+            mathjax=True,
+        ),
+        html.Button(
+            "Download Code", id="btn-download-derivatives", className="btn-download"
+        ),
+        dcc.Download(id="download-derivatives"),
+    ]
+
+    return tab5Layout
 
 
-def cube(x):
-    return x**3
-
-
-def poly(x):
-    return 2 * (x**2) + 4
-
-
-def log(x):
-    return np.log(x)
-
-
+#######################################
+# Derivatives Callbacks
+#######################################
 @my_app.callback(
     Output(component_id="functions-graph", component_property="figure"),
     Output(component_id="derivative-graph", component_property="figure"),
---
+++ utils/stats/non_linear.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def square(x):
+    return x**2
+
+
+def cube(x):
+    return x**3
+
+
+def poly(x):
+    return 2 * (x**2) + 4
+
+
+def log(x):
+    return np.log(x)
